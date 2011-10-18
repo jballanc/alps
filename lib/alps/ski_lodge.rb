801ca7ce905ce438940d34c154d018cfc4725770
Joshua Ballanco <joshua.ballanco@patch.com>
2011-10-18 14:54:28 -0400

    SkiLodge to coordinate skiers

---
+++ lib/alps/ski_lodge.rb
@@ -0,0 +1,25 @@
+class SkiLodge
+  attr_reader :longest_run
+  def initialize
+    @skiers = []
+    @longest_run = 0
+  end
+
+  def report_run(length)
+    if length > @longest_run
+      @longest_run = length
+    end
+  end
+
+  def ski_map(trail_map)
+    trail_map.each_mountain do |mountain, pos|
+      @skiers << Skier.new(mountain, pos, self)
+    end
+
+    until @skiers.empty?
+      @skiers.map! do |skier|
+        skier.ski_to(trail_map.mountains_adjacent_to(skier.pos))
+      end.flatten!.compact!
+    end
+  end
+end
